499ed296baac3fac707b52d98bb70cdcca3a9281
Dan Birle <dan.birle@letsbloom.com>
2022-05-20 20:05:01 +0300

    dxasdc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import "./App.css";
 
 function App() {
-  function lsTest() {
-    var test = "test";
-    try {
-      localStorage.setItem(test, test);
-      localStorage.removeItem(test);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  if (lsTest() === true) {
-    alert("available");
-  } else {
-    // unavailable
-    alert("unavailable");
-  }
-
   return (
     <div className="App">
       <p>This is iFrame cobiro test!</p>
