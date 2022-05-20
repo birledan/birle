df7e3db14c6f2074dab29fb069fd3148df862796
Dan Birle <dan.birle@letsbloom.com>
2022-05-20 18:55:37 +0300

    ascxas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
 
 function App() {
-  // const iframe = document.getElementsByTagName("iframe");
-  // const iWindow = iframe.contentWindow;
-  // const iDocument = iWindow.document;
-  // console.log("aaa= ", iframe);
+  function lsTest() {
+    var test = "test";
+    try {
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  if (lsTest() === true) {
+    alert("available");
+  } else {
+    // unavailable
+    alert("unavailable");
+  }
 
   return (
     <div className="App">
       <p>This is iFrame cobiro test!</p>
       <iframe
+        id="bloomID"
         name="bloom"
         title="bloom"
         src="https://smokingcanary.app.letsbloom.com/partners/cobiro/apply"
