f7e9febe54dd00287bef2c3a870665574cf85eff
Dan Birle <dan.birle@letsbloom.com>
2022-05-31 15:30:33 +0300

    change style

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ function App() {
     <div className="App">
       <p>This is iFrame cobiro test in smokingcanary!!</p>
       <iframe
-        id="bloomID"
         name="bloom"
         title="bloom"
         src="https://smokingcanary.app.letsbloom.com/partners/cobiro/apply"
         width="800"
         height="930"
-        style={{ border: "none" }}
+        style={{
+          maxWidth: "900px",
+          margin: "75px auto 40px",
+          border: "1px solid #c4c4c4",
+          borderRadius: "8px",
+          padding: "40px",
+        }}
       ></iframe>
     </div>
   );
